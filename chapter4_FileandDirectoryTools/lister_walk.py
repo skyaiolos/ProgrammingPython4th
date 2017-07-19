b37111c30242326b43ab62e9ddf58c69a8341c9b
skyaiolos <skyaiolos@aliyun.com>
2017-07-19 21:51:28 +0800

    chapter 4 File and Dicectory Tools

---
+++ chapter4_FileandDirectoryTools/lister_walk.py
@@ -0,0 +1,14 @@
+"list file tree with os.walk"
+
+import sys, os
+
+def lister(root):                                           # for a root dir
+    for (thisdir, subshere, fileshere) in os.walk(root):    # generate dirs in tree
+        print('[' + thisdir + ']')
+        for fname in fileshere:                             # print files in this dir
+            path = os.path.join(thisdir, fname)             # add dir name prefix
+            print(path)
+
+if __name__ == '__main__':
+    lister(sys.argv[1])                                     # dir name in cmdline
+# python lister_walk.py C:\temp\test
